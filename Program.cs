2781efcbdc2a2d555c7cde56142b458e01e40184
Faiza <faraz_ul_islam@yahoo.com>
2024-10-02 13:21:13 +0100

    forEach with if elseif else

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿Random random = new Random();
+string[] orderIDs = new string[5];
+
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    // Get a random value that equates to ASCII letters A through E
+    int preFixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(preFixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(0, 1000).ToString("000");
+    // Combine the prefix and suffix together, then assign to current OrderID
+    orderIDs[i] = prefix + suffix;
+}
+// Print out each orderID
+foreach (string order in orderIDs)
+{
+    Console.WriteLine(order);
+}
+Console.WriteLine("****************");
+
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
+
+
+Console.WriteLine("*************************");
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
+Console.WriteLine("/*************************/");
+
+
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+int[] studentScores = new int[10];
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+foreach (string names in studentNames)
+{
+    string currentStudent = names;
+    //Console.WriteLine($"Names are: {names}");
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+    else if (currentStudent == "Andrew")
+    {
+        studentScores = andrewScores;
+    }
+    else if (currentStudent == "Emma")
+    {
+        studentScores = emmaScores;
+    }
+    else if (currentStudent == "Logan")
+    {
+        studentScores = loganScores;
+    }
+
+    int sophiaSum = 0;
+    decimal sophiaScore;
+
+    foreach (int score in sophiaScores)
+    {
+        // add the exam score to the sum
+        sophiaSum += score;
+        // or sophiaSum = sophiaSum + score;
+
+    }
+    sophiaScore = (decimal)sophiaSum / currentAssignments;
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+
+
+}
+
+/*
+Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");*/
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
